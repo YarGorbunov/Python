7b77c573b52a51ab6b89fed66ef5bc61cc5613ac
YarGorbunov <yaroslav-gorbunov99@mail.ru>
2024-02-02 19:11:07 +0300

    made task2-4 first task

---
+++ lab1/task2-4.py
@@ -0,0 +1,13 @@
+def check(string):
+    s = string
+    i = 0
+    while i < len(s):
+        #if i >= len(s): break
+        if s[i] < "a" or s[i] > "z":
+            s = s[:i] + s[i + 1:]
+            i -= 1
+        i += 1
+    for i in range(1,len(s)):
+        if s[i-1] > s[i]:
+            return False
+    return True
