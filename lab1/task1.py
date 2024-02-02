226ca551415a72084d6e19f78d738dc62a315113
YarGorbunov <yaroslav-gorbunov99@mail.ru>
2024-02-02 16:45:11 +0300

    made func3

--- lab1/task1.py
+++ lab1/task1.py
@@ -22,3 +22,28 @@ def func2(num):
         num = int(num / 10)
     return s
 
+def func3(num):
+    l = []
+    for i in range(1, num+1):
+        if num % i == 0:
+            l.append(i)
+    k = 0
+    while num:
+        k += 1
+        num = int(num / 10)
+    m = []
+    for i in range(1, k+1):
+        if k % i == 0:
+            m.append(i)
+    r = []
+    print("l: " + str(l))
+    print("m: " + str(m))
+    for i in l:
+        flag = 1
+        for j in m:
+            if (i % j == 0) and ((i != 1) and (j != 1)):
+                flag = 0
+                break
+        if flag:
+            r.append(i)
+    return r
