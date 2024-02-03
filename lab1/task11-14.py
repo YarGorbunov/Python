454497dfb3f0f684053894d3c1d419784da7ad51
YarGorbunov <yaroslav-gorbunov99@mail.ru>
2024-02-03 19:25:46 +0300

    made task11-14 third task

--- lab1/task11-14.py
+++ lab1/task11-14.py
@@ -15,3 +15,11 @@ def sort2(l):
         return (avgASCII(string) - a) ** 2
     return sorted(l, key=keyFunc)
 
+def sort3(l):
+    def keyFunc(string):
+        k = 0
+        for i in range(1,len(string)):
+            if ((string[i-1] in ["a", "e", "y", "u", "i", "o"]) and (string[i] not in ["a", "e", "y", "u", "i", "o"])) or ((string[i-1] not in ["a", "e", "y", "u", "i", "o"]) and (string[i] in ["a", "e", "y", "u", "i", "o"])):
+                k += 1
+        return k
+    return sorted(l, key=keyFunc)
