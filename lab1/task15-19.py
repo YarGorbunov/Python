6e31ed404da6688dd2ce449a5a0e7a7fb15b5040
YarGorbunov <yaroslav-gorbunov99@mail.ru>
2024-02-03 20:08:22 +0300

    made task15-19 third task

--- lab1/task15-19.py
+++ lab1/task15-19.py
@@ -8,3 +8,16 @@ def func2(l, a, b):
             k += 1
     return k
 
+def func3(l):
+    minEl = min(l)
+    first = -1
+    last = -1
+    for i in range(0,len(l)):
+        if l[i] == minEl:
+            if first == -1: first = i
+            else: last = i
+    k = 0
+    for i in range(0,len(l)):
+        if i > first and i < last: k += 1
+    return k
+
